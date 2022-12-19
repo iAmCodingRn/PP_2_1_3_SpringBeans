2703e4a1c2a225ff125cedf50b8a43bdf529694c
iAmCodingRn <111708435+iAmCodingRn@users.noreply.github.com>
2022-12-19 08:57:17 +0300

    first commit (third try Spring_Beans after mentor's checking. Dog class wasn't added to GitRepository at the moment of creation the class)

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,7 +1,4 @@
 package app.config;
-import app.model.Dog;
-import app.model.Timer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
